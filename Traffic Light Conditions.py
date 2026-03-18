03a827280dac1b77ad38660748b40455c26d0263
Pradeep Kumar <dipushipta@gmail.com>
2026-03-18 15:32:06 +0530

    Add Traffic Light Conditions Python program
    This project is a simple Python program that simulates traffic light behavior using conditional statements. The user inputs a traffic light color (Red, Yellow, or Green), and the program displays the corresponding action such as Stop, Get Ready, or Go. This project helps in understanding basic Python concepts like user input and if-elif-else conditions.

---
+++ Traffic Light Conditions.py
@@ -0,0 +1,9 @@
+Light =  input ("Enter the traffic light color (Red/Yellow/Green)")
+if Light == "Red":
+    print("Stop")
+elif Light == "Yellow":
+    print("Get Ready")
+elif Light == "Green":
+    print("Go")
+else:
+    print("Invalid Color!, Please Enter valid color")
